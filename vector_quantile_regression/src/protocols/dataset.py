772b14f882b79a1ccf3fddcc648d830c084e3064
VldKnd <kondratyev.w.i@gmail.com>
2025-07-23 19:27:42 +0700

    feat(vqr): add protocols for future functions

---
+++ vector_quantile_regression/src/protocols/dataset.py
@@ -0,0 +1,32 @@
+import torch
+from typing import Union
+
+class Dataset:
+    def __init__(self, *args, **kwargs):
+        ...
+
+    def sample_joint(self, n_points: int) -> Union[torch.Tensor, torch.Tensor]:
+        """
+            Returns:
+            (x, y) - Union[torch.Tensor[n, k], torch.Tensor[n, p]]
+        """
+        ...
+
+    def sample_covariates(self, n_points: int) -> torch.Tensor:
+        """
+            Returns:
+            (x, y) - Union[torch.Tensor[n, k], torch.Tensor[n, p]]
+        """
+        ...
+
+    def sample_conditional(self, n_points: int, x: torch.Tensor) -> torch.Tensor:
+        """Sample conditional distribution from y|x.
+
+        Args:
+            n_points (int): number of points
+            x (torch.Tensor[1, k]): covariate
+
+        Returns:
+            torch.Tensor[n, p]: Conditional sample
+        """
+        ...
