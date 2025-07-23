d5723c6f92865ed2d623522d59437a7b027d816e
VldKnd <kondratyev.w.i@gmail.com>
2025-07-23 19:33:28 +0700

    feat(vqr): drop old folder

--- vector_quantile_regression/src/distributions.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def torch_sphere_uniform(n, d, **kwargs):
-    """Generate n points inside the d-dimensional sphere."""
-    random_vectors = torch.randn(n, d, **kwargs)
-    vectors_norms = torch.norm(random_vectors, dim=1, keepdim=True)
-    radius = torch.pow(torch.rand(n, 1, **kwargs), 1. / d)
-    return radius * random_vectors / vectors_norms
