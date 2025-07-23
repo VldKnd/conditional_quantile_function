c9650d6254f6768efbcc9fe0f22e9ec80125a652
VldKnd <kondratyev.w.i@gmail.com>
2025-07-23 19:29:46 +0700

    feat(vqr): move all to root

---
+++ README.md
@@ -0,0 +1,36 @@
+### Conditional Quantile Estimation
+This project contains experiments for conditional quantile estimation using two different models. The core logic and helper functions are located in the src folder, while the Jupyter notebooks detailing the experiments can be found in the notebooks folder.
+
+### Installation
+You can manage the Python dependencies for this project using uv or pip.
+
+<details>
+<summary><b>Optional: Installing uv</b></summary>
+
+uv is an extremely fast Python package installer and resolver. If you'd like to use it, you can install it with the following command:
+```bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+```
+</details>
+
+### Project Dependencies
+
+
+**Using uv:**
+```bash
+uv sync
+```
+
+**Using pip:**
+
+Create a virtual environment:
+```bash
+python -m venv .venv
+source .venv/bin/activate
+```
+Install the dependencies using uv or pip:
+```bash
+pip install -r requirements.txt
+```
+### Usage
+The notebooks folder contains the primary experiments.
---
+++ src/picnn.py
@@ -0,0 +1,117 @@
+import torch
+from torch import nn
+from torch import Tensor
+
+class PosLinear(torch.nn.Linear):
+    def forward(self, x: Tensor) -> Tensor:
+        gain = 1 / x.size(-1)
+        return nn.functional.linear(x, torch.nn.functional.softplus(self.weight), self.bias) * gain
+
+class PICNN(nn.Module):
+    def __init__(self,
+            x_dimension: int,
+            y_dimension: int,
+            u_dimension: int,
+            z_dimension: int,
+            output_dimension: int,
+            number_of_hidden_layers: int
+        ):
+        super(PICNN, self).__init__()
+
+        # Activations:
+        self.z_activation = nn.Softplus()
+        self.u_activation = nn.Softplus()
+        self.positive_activation = nn.Softplus()
+
+        # First layer
+        self.first_linear_layer_tilde = nn.Linear(x_dimension, u_dimension)
+        self.first_linear_layer_yu = nn.Linear(x_dimension, y_dimension)
+        self.first_linear_layer_y = nn.Linear(y_dimension, z_dimension, bias=False)
+        self.first_linear_layer_u = nn.Linear(x_dimension, z_dimension, bias=False)
+
+        # Iterations:
+        self.linear_layer_tilde = nn.ModuleList([
+                nn.Linear(u_dimension, u_dimension)
+                for _ in range(number_of_hidden_layers)
+        ])
+        self.linear_layer_uz = nn.ModuleList([
+            nn.Linear(u_dimension, z_dimension)
+                for _ in range(number_of_hidden_layers)
+        ])
+        self.linear_layer_z = nn.ModuleList([
+            PosLinear(z_dimension, z_dimension)
+                for _ in range(number_of_hidden_layers)
+        ])
+        self.linear_layer_uy = nn.ModuleList([
+            nn.Linear(u_dimension, y_dimension)
+                for _ in range(number_of_hidden_layers)
+        ])
+        self.linear_layer_y = nn.ModuleList([
+            nn.Linear(y_dimension, z_dimension, bias=False)
+                for _ in range(number_of_hidden_layers)
+        ])
+        self.linear_layer_u = nn.ModuleList([
+            nn.Linear(u_dimension, z_dimension, bias=False)
+                for _ in range(number_of_hidden_layers)
+        ])
+        self.number_of_hidden_layers = number_of_hidden_layers
+
+        # Last layer:
+        self.last_linear_layer_uz = nn.Linear(u_dimension, z_dimension)
+        self.last_linear_layer_z = PosLinear(z_dimension, output_dimension)
+        self.last_linear_layer_uy = nn.Linear(u_dimension, y_dimension)
+        self.last_linear_layer_y = nn.Linear(y_dimension, output_dimension, bias=False)
+        self.last_linear_layer_u = nn.Linear(u_dimension, output_dimension, bias=False)
+
+
+    def forward(self, x, y):
+        # First layer:
+        u = self.u_activation(
+            self.first_linear_layer_tilde(x)
+        )
+        z = self.z_activation(
+            self.first_linear_layer_y(
+                y * self.first_linear_layer_yu(x)
+            ) +
+            self.first_linear_layer_u(x)
+        )
+
+        # Iterations:
+        for iteration_number in range(self.number_of_hidden_layers):
+            u, z = (
+                self.u_activation(
+                    self.linear_layer_tilde[iteration_number](u)
+                ),
+                self.z_activation(
+                    self.linear_layer_z[iteration_number](
+                        z * self.positive_activation((self.linear_layer_uz[iteration_number](u)))
+                    ) + \
+                    self.linear_layer_y[iteration_number](
+                        y * self.linear_layer_uy[iteration_number](u)
+                    ) + \
+                    self.linear_layer_u[iteration_number](u)
+                )
+            )
+
+        # Last layer:
+        output = self.last_linear_layer_z(
+            z * self.positive_activation(self.last_linear_layer_uz(u))
+        ) + \
+        self.last_linear_layer_y(
+            y * self.last_linear_layer_uy(u)
+        ) + \
+        self.last_linear_layer_u(u)
+
+        return output
+
+class SCPICNN(PICNN):
+    """Strongly convex variant of PICNN.
+    alpha: regularization parameter
+    """
+    def __init__(self, alpha: float, *args, **kwargs):
+        super(SCPICNN, self).__init__(*args, **kwargs)
+        self.alpha = alpha
+
+    def forward(self, x, y):
+        output = super().forward(x, y)
+        return output + 0.5 * self.alpha * torch.norm(y, dim=-1, keepdim=True)**2
