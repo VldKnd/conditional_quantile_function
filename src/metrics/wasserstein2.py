b0d02847e34a3cf30eb5e60debd69057d3b372ab
VldKnd <kondratyev.w.i@gmail.com>
2025-07-25 14:32:29 +0700

    feat(vqr): modfiy w2

--- src/metrics/wasserstein2.py
+++ src/metrics/wasserstein2.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     for alpha in [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1]:
         ground_truth = torch.rand(1000, 5)
         approximation = torch.rand(1000, 5) * alpha + 10 * (1 - alpha)
-        print(f"{wassertein2(ground_truth, approximation)}")
+        print(f"alpha: {alpha}, wassertein2: {wassertein2(ground_truth, approximation)}")
