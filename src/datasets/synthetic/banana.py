bbfae58cc0f7db81bd7c7d8333318fdf7919bc64
VldKnd <kondratyev.w.i@gmail.com>
2025-07-30 20:04:57 +0700

    feat(vqr): add sending back the quantile functions

--- src/datasets/synthetic/banana.py
+++ src/datasets/synthetic/banana.py
@@ -1,6 +1,7 @@
 import torch
 
-from src.protocols.dataset import Dataset
+from typing import Tuple
+from protocols.dataset import Dataset
 
 
 class BananaDataset(Dataset):
@@ -13,27 +14,51 @@ class BananaDataset(Dataset):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rng = torch.Generator()
-        self.rng.manual_seed(self.seed)
 
-    def sample_covariates(self, n_points):
-        self.rng.manual_seed(self.seed)
-        x = torch.rand(size=(n_points, 1), generator=self.rng) * 2 + 0.5
+    def sample_covariates(self, n_points: int) -> torch.Tensor:
+        """
+        Sample the covariates from the uniform distribution between 1 and 5.
+        """
+        x = torch.rand(size=(n_points, 1)) * 2 + 0.5
         return x
 
-    def sample_conditional(self, n_points, x):
-        self.rng.manual_seed(self.seed)
-        u = torch.randn(size=(n_points, 2), generator=self.rng)
+    def sample_conditional(self, n_points: int, X: torch.Tensor) -> torch.Tensor:
+        """
+        Sample the conditional distribution of the response given the covariates.
+        """
+        U = torch.randn(size=(X.shape[0], n_points, 2))
+        X_unsqueezed = X.unsqueeze(1)
         y = torch.concatenate(
             [
-                u[:, 0:1] * x,
-                u[:, 1:2] / x + (u[:, 0:1] ** 2 + x**3),
+                U[:, :, 0:1] * X_unsqueezed,
+                U[:, :, 1:2] / X_unsqueezed + (U[:, :, 0:1] ** 2 + X_unsqueezed**3),
             ],
-            dim=1,
+            dim=-1,
         )
         return y
 
-    def sample_joint(self, n_points):
-        x = self.sample_covariates(n_points=n_points)
-        y = self.sample_conditional(n_points=n_points, x=x)
-        return x, y
+    def sample_joint(self, n_points: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Sample the joint distribution of the covariates and the response.
+        """
+        X = self.sample_covariates(n_points=n_points)
+        U = torch.randn(size=(X.shape[0], 2))
+        Y = torch.concatenate(
+            [
+                U[:, 0:1] * X,
+                U[:, 1:2] / X + (U[:, 0:1] ** 2 + X**3),
+            ],
+            dim=-1,
+        )
+
+        return X, Y
+
+    def pushbackwards_Y_given_X(self, Y: torch.Tensor, X: torch.Tensor) -> torch.Tensor:
+        """
+        Push backwards the conditional distribution of the response given the covariates.
+        """
+        assert Y.shape[0] == X.shape[0], "The number of rows in Y and X must be the same."
+        U0 = Y[:, 0:1] / X
+        U1 = ( Y[:, 1:2] - (U0 ** 2 + X**3) )* X
+        U = torch.concatenate([U0, U1], dim=-1)
+        return U
--- src/infrastructure/testing.py
+++ src/infrastructure/testing.py
@@ -1,10 +1,10 @@
 from protocols.pushforward_operator import PushForwardOperator
 from protocols.dataset import Dataset
 import torch
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple, List
 from metrics.wasserstein2 import wassertein2
 
-def benchmark_wasserstein2_on_synthetic_data(model: PushForwardOperator, dataset: Dataset, device_and_dtype_specifications: Dict, path_to_load_model: Optional[str] = None) -> float:
+def benchmark_wasserstein2_on_synthetic_data(model: PushForwardOperator, dataset: Dataset, device_and_dtype_specifications: Dict, path_to_load_model: Optional[str] = None, covariates: Optional[torch.Tensor] = None) -> Tuple[List[float], torch.Tensor]:
     """
     Benchmark the Wasserstein-2 distance between the ground truth and the model's pushforward of a random sample of the dataset.
 
@@ -13,27 +13,32 @@ def benchmark_wasserstein2_on_synthetic_data(model: PushForwardOperator, dataset
         dataset (Dataset): The dataset to use for the benchmark.
         device_and_dtype_specifications (Dict): The device and dtype specifications to use for the benchmark.
         path_to_load_model (Optional[str], optional): The path to the model to load. Defaults to None.
-
+        covariates (Optional[torch.Tensor], optional): The tensor of covariates to use for the benchmark. Defaults to None.
     Returns:
-        float: The Wasserstein-2 distance between the ground truth and the model's pushforward of a random sample of the dataset.
+        Tuple[List[float], torch.Tensor]: The Wasserstein-2 distance between the ground truth and the model's pushforward of a random sample of the dataset.
     """
-    X_dataset, Y_dataset = dataset.sample_joint(n_points=1000)
-    U_dataset = torch.randn_like(Y_dataset)
+    if covariates is None:
+        covariates, _ = dataset.sample_joint(n_points=1000)
 
-    X_dataset = X_dataset.to(**device_and_dtype_specifications)
+    Y_dataset = dataset.sample_conditional(n_points=covariates.shape[0], X=covariates)
+    metric_values = []
+
+    covariates = covariates.to(**device_and_dtype_specifications)
     Y_dataset = Y_dataset.to(**device_and_dtype_specifications)
-    U_dataset = U_dataset.to(**device_and_dtype_specifications)
     model.to(**device_and_dtype_specifications)
 
     if path_to_load_model is not None:
         model.load(path_to_load_model)
 
-    metric_value = wassertein2(
-        ground_truth=Y_dataset,
-        approximation=model.push_forward_u_given_x(
-            U=U_dataset,
-            X=X_dataset
+    for i in range(covariates.shape[0]):
+        U_dataset = torch.randn_like(Y_dataset[i, :, :])
+        metric_value = wassertein2(
+            ground_truth=Y_dataset[i, :, :],
+            approximation=model.push_forward_u_given_x(
+                U=U_dataset,
+                X=covariates[i:i+1, :].repeat(U_dataset.shape[0], 1)
+            )
         )
-    )
+        metric_values.append(metric_value)
 
-    return metric_value
+    return metric_values, covariates
