6e07780fe607644af81f9237137dbcebfa32f61f
VldKnd <kondratyev.w.i@gmail.com>
2025-07-31 11:42:16 +0700

    feat(vqr): add function to extract total number of parameters

--- src/utils.py
+++ src/utils.py
@@ -81,3 +81,13 @@ def plot_potentials_from_banana_dataset(model: PushForwardOperator, device_and_d
     plt.tight_layout(rect=[0, 0, 1, 0.96])
     plt.show()
 
+def get_total_number_of_parameters(model: torch.nn.Module) -> int:
+    """Get the total number of parameters in the model.
+
+    Args:
+        model (torch.nn.Module): The model.
+
+    Returns:
+        int: The total number of parameters in the model.
+    """
+    return sum(p.numel() for p in model.parameters())
