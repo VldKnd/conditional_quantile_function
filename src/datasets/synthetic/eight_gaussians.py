3ea0fd084922874483f4dbbd559c9d5dcc8736ec
Alexander Fishkov <4907049+jetsnguns@users.noreply.github.com>
2025-08-06 22:07:10 +0300

    feat(8g) fix eight gaussians generation, add visualization

--- src/datasets/synthetic/eight_gaussians.py
+++ src/datasets/synthetic/eight_gaussians.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import torch
 
@@ -8,19 +9,37 @@ class EightGaussians(Dataset):
 
     def sample_covariates(self, n_points: int) -> torch.Tensor:
         x = torch.rand(size=(n_points, 1)) * 2 + 0.5
-        return x
+        return x.to(**self.tensor_prameters)
 
     def sample_conditional(self, n_points, x):
+        n_x, d = x.shape
+        print(f"{x.shape=}")
         scale = 4.
         centers = [(1, 0), (-1, 0), (0, 1), (0, -1),
-                   (1. / np.sqrt(2), 1. / np.sqrt(2)),
-                   (1. / np.sqrt(2), -1. / np.sqrt(2)),
-                   (-1. / np.sqrt(2), 1. / np.sqrt(2)),
-                   (-1. / np.sqrt(2), -1. / np.sqrt(2))]
-        centers = [(scale * x, scale * y) for x, y in centers]
+                   (1. / math.sqrt(2), 1. / math.sqrt(2)),
+                   (1. / math.sqrt(2), -1. / math.sqrt(2)),
+                   (-1. / math.sqrt(2), 1. / math.sqrt(2)),
+                   (-1. / math.sqrt(2), -1. / math.sqrt(2))]
+        centers = torch.tensor(data=[(scale * x, scale * y)
+                                     for x, y in centers],
+                               **self.tensor_prameters)
 
-        y = []
-        indices = []
+        idx = np.random.randint(8, size=(n_x, n_points))
+        print(idx[:5])
+
+        selected_centers = centers[idx]
+
+        print(f"{selected_centers.shape=}")
+
+        points = torch.randn(size=(n_x, n_points, 2)) * 0.5
+
+        y = (points + selected_centers) * torch.abs(x).reshape(-1, 1,
+                                                               1) / 1.414
+
+        return y
+
+
+"""         indices = []
         for i in range(n_points):
             point = np.random.randn(2) * 0.5
             idx = np.random.randint(8)
@@ -33,3 +52,4 @@ class EightGaussians(Dataset):
         y /= 1.414
         y_tensor = torch.from_numpy(y) * torch.abs(x)
         return y_tensor
+ """
