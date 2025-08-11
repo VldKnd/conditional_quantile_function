0201bba0e5232ab651862f5a2c05a98ed73366c9
Alexander Fishkov <4907049+jetsnguns@users.noreply.github.com>
2025-08-11 20:52:03 +0300

    Add missing dataset methods

--- src/datasets/synthetic/eight_gaussians.py
+++ src/datasets/synthetic/eight_gaussians.py
@@ -11,9 +11,12 @@ class EightGaussians(Dataset):
         x = torch.rand(size=(n_points, 1)) * 2 + 0.5
         return x.to(**self.tensor_prameters)
 
+    def meshgrid_of_covariates(self, n_points_per_dimension):
+        return np.linspace(0.5, 2.5, num=n_points_per_dimension)
+
     def sample_conditional(self, n_points, x):
         n_x, d = x.shape
-        print(f"{x.shape=}")
+        #print(f"{x.shape=}")
         scale = 4.
         centers = [(1, 0), (-1, 0), (0, 1), (0, -1),
                    (1. / math.sqrt(2), 1. / math.sqrt(2)),
@@ -25,11 +28,11 @@ class EightGaussians(Dataset):
                                **self.tensor_prameters)
 
         idx = np.random.randint(8, size=(n_x, n_points))
-        print(idx[:5])
+        #print(idx[:5])
 
         selected_centers = centers[idx]
 
-        print(f"{selected_centers.shape=}")
+        #print(f"{selected_centers.shape=}")
 
         points = torch.randn(size=(n_x, n_points, 2)) * 0.5
 
@@ -38,18 +41,7 @@ class EightGaussians(Dataset):
 
         return y
 
-
-"""         indices = []
-        for i in range(n_points):
-            point = np.random.randn(2) * 0.5
-            idx = np.random.randint(8)
-            center = centers[idx]
-            point[0] += center[0]
-            point[1] += center[1]
-            y.append(point)
-            indices.append(idx)
-        y = np.array(y, dtype='float32')
-        y /= 1.414
-        y_tensor = torch.from_numpy(y) * torch.abs(x)
-        return y_tensor
- """
+    def sample_joint(self, n_points):
+        x = self.sample_covariates(n_points=n_points)
+        y = self.sample_conditional(n_points=1, x=x).squeeze(dim=1)
+        return x, y
