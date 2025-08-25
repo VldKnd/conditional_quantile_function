2bc807a43ef91db6d15a61fba712d6a51e410126
VldKnd <kondratyev.w.i@gmail.com>
2025-08-25 12:27:04 +0400

    fix(vqr): fix datasets

--- src/datasets/synthetic/custom/__init__.py
+++ src/datasets/synthetic/custom/__init__.py
@@ -1,6 +1,6 @@
-from datasets.synthetic.other.banana import BananaDataset
-from datasets.synthetic.other.tictac import TicTacDataset
-from datasets.synthetic.other.not_conditional_banana import NotConditionalBananaDataset
+from datasets.synthetic.custom.banana import BananaDataset
+from datasets.synthetic.custom.tictac import TicTacDataset
+from datasets.synthetic.custom.not_conditional_banana import NotConditionalBananaDataset
 
 __all__ = [
     "BananaDataset",
