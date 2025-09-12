a89bfbdb7cfdd13e08226f3596aa3462b47ad970
Alexander Fishkov <4907049+jetsnguns@users.noreply.github.com>
2025-09-12 03:01:42 +0300

    refactor runner script

--- scripts/run_all_conformal_slurm.sh
+++ scripts/run_all_conformal_slurm.sh
@@ -1,6 +1,12 @@
 datasets=("rf1" "rf2" "scm1d" "scm20d" "sgemm" "bio" "blog")
-seeds=(0 1 2 3 4)
+seeds=(0 1 2 3 4 5 6 7 8 9)
 MAX_JOBS=64
+LOG_DIR="./logs"
+
+declare -a SBATCH_OPTS=(
+    [0]="--account=$ACCOUNT"
+    [1]="--partition=$PARTITION"
+)
 
 # Function to limit concurrent jobs
 # squeue -h -t pending,running -r
@@ -14,6 +20,9 @@ function wait_for_jobs {
 for seed in "${seeds[@]}"; do
     for dataset in "${datasets[@]}"; do
         wait_for_jobs "$MAX_JOBS"
-        sbatch ./scripts/slurm/conformal.sbatch.sh $dataset $seed --all
+        SBATCH_OPTS[2]="--job-name=conf_$dataset_$seed"
+        SBATCH_OPTS[3]="--output=$LOG_DIR/conformal_$dataset_$seed.log"
+        SBATCH_OPTS[4]="--error="$LOG_DIR/conformal_$dataset_$seed.err"
+        sbatch "${SBATCH_OPTS[@]}" ./scripts/slurm/conformal.sbatch.sh $dataset $seed --all
     done
 done
