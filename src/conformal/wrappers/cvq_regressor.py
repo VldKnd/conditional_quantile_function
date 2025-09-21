57c7dccc237502454f89cc5537eeba4d706ffd3e
VldKnd <kondratyev.w.i@gmail.com>
2025-09-21 14:00:33 +0400

    feat(vqr): udpate hessian to estimate on the phi(u, x)

--- src/conformal/wrappers/cvq_regressor.py
+++ src/conformal/wrappers/cvq_regressor.py
@@ -42,6 +42,7 @@ class BaseVQRegressor(ScoreCalculator):
     batch_size: int
     n_epochs: int
     learning_rate: float = 0.01
+    potential_to_estimate_with_neural_network = "u"
     dtype: torch.dtype = torch.float64
     betas: tuple[float, float] = (0.5, 0.5)
     weight_decay: float = 1e-4
@@ -73,8 +74,6 @@ class BaseVQRegressor(ScoreCalculator):
         n = X.shape[0]
         U = torch.zeros((n, self.response_dimension), dtype=self.dtype)
         X_tensor = torch.tensor(X, dtype=self.dtype)
-        #dataset = TensorDataset(X_tensor, U)
-        #dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
         Y = self.model.push_u_given_x(x=X_tensor, u=U)
         return Y.numpy(force=True)
 
@@ -94,20 +93,24 @@ class CVQRegressor(BaseVQRegressor):
             response_dimension=self.response_dimension,
             hidden_dimension=self.hidden_dimension,
             number_of_hidden_layers=self.number_of_hidden_layers,
-            potential_to_estimate_with_neural_network="y",
+            potential_to_estimate_with_neural_network=self.
+            potential_to_estimate_with_neural_network,
         ).to(self.dtype)
 
-    def predict_logdet_hessian_potential(self, X, Y, batch_size: int | None = None):
+    def compute_logdet_hessian(
+        self,
+        condition: torch.Tensor,
+        tensor: torch.Tensor,
+        batch_size: int | None = None
+    ):
         _compute_batch_hessian = torch.vmap(
             func=hessian(self.model.potential_network, argnums=1),
             in_dims=0,
             chunk_size=batch_size or self.batch_size
         )
-        hessians = _compute_batch_hessian(
-            torch.tensor(X, dtype=self.dtype), torch.tensor(Y, dtype=self.dtype)
-        )[:, 0].detach()
-        logdet_h = torch.logdet(hessians).squeeze().detach().numpy(force=True)
-        return logdet_h
+        hessians = _compute_batch_hessian(condition, tensor)[:, 0].detach()
+        logdet_hessians = torch.logdet(hessians).squeeze().detach().numpy(force=True)
+        return logdet_hessians
 
     def calculate_scores(
         self,
@@ -121,17 +124,29 @@ class CVQRegressor(BaseVQRegressor):
         Returns: quantiles, ranks, log_p
 
         """
-        n, m = X.shape
         _, d = Y.shape
         quantiles = self.predict_quantile(X, Y)
         ranks = np.linalg.norm(quantiles, axis=-1)
-        log_p_U = multivariate_normal.logpdf(quantiles, mean=np.zeros(d))
-        logdet_h = self.predict_logdet_hessian_potential(X, Y, batch_size=batch_size)
-        return {
-            "MK Quantile": quantiles,
-            "MK Rank": ranks,
-            "Log Density": log_p_U + logdet_h
-        }
+        X_tensor = torch.tensor(X, dtype=self.dtype)
+        Y_tensor = torch.tensor(Y, dtype=self.dtype)
+
+        if self.potential_to_estimate_with_neural_network == "y":
+            logdet_hessians = self.compute_logdet_hessian(
+                condition=X_tensor, tensor=Y_tensor, batch_size=batch_size
+            )
+
+            log_density_in_u = multivariate_normal.logpdf(quantiles, mean=np.zeros(d))
+            log_density = log_density_in_u + logdet_hessians
+
+        if self.potential_to_estimate_with_neural_network == "u":
+            logdet_hessians = self.compute_logdet_hessian(
+                condition=X_tensor, tensor=Y_tensor, batch_size=batch_size
+            )
+
+            log_density_in_u = multivariate_normal.logpdf(quantiles, mean=np.zeros(d))
+            log_density = log_density_in_u - logdet_hessians
+
+        return {"MK Quantile": quantiles, "MK Rank": ranks, "Log Density": log_density}
 
 
 @dataclass
